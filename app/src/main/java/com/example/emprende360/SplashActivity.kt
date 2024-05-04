7cbe44f96bb5fca3954eba80a273a7a15cbce3b3
LMiguelx <144188401+LMiguelx@users.noreply.github.com>
2024-05-04 12:56:01 -0500

    Splash

---
+++ app/src/main/java/com/example/emprende360/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.example.emprende360
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        actionBar?.hide()
+
+        val splashTimer = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                    val intent = Intent(applicationContext, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        splashTimer.start()
+    }
+}
